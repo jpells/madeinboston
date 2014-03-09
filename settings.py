4dcf49ca52f92f1826141469928aa8a7acea71dc
James Pells <jimmy@jamespells.com>
2014-03-09 18:25:53 -0400

    start customtheme app for templates and static resources

--- settings.py
+++ settings.py
@@ -299,6 +299,7 @@ TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
 ################
 
 INSTALLED_APPS = (
+    "customtheme",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
